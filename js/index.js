3c256fb03953c586c8e73328a36dbd39432044e7
chamindujs <chamindujs@gmail.com>
2023-04-18 11:43:49 +0530

    Add all animations to 1 file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 const homeBg = document.querySelector(".homeBg");
 let documentWidth = window.innerWidth;
+let scrollPos = window.scrollY;
+const navBar = document.querySelector(".topNavigation");
 
 window.onscroll = function(e) {
+    // Home phone animation
     homeBg.style.width = `${scrollY + (documentWidth*0.8)}px`;
+
+    // Navbar animation
+    if (scrollPos < scrollY) {
+        navBar.classList.add("fixed");
+    } else {
+        navBar.classList.remove("fixed")
+    }
+    scrollPos = window.scrollY;
 }
