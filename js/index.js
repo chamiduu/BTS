1e7795303d64363abdece3b0347db9114a43b462
chamindujs <chamindujs@gmail.com>
2023-04-22 21:23:15 +0530

    carousel from Ohio

--- js/index.js
+++ js/index.js
@@ -17,3 +17,19 @@ window.onscroll = function(e) {
     }
     scrollPos = window.scrollY;
 }
+
+// the godforbidden carousel
+// idk how the fuck i did this but it works somehow (i'm a retard)
+i = 1;
+let slides = document.querySelectorAll(".carouselCard");
+
+function switchSlidesRight(e) {
+    slides[i].classList.remove("active"); // 1, 2
+    if (i == 2) {
+        i = -1;
+    }
+    targetSlide = slides[i+1] // 2, 0, 1, 
+    targetSlide.classList.add("active");
+    i += 1 //2
+    
+}
